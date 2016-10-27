9bdaeb19748b8d9d6416ecb9ea4ed74908404fd7
Jon Catanio <joncatanio@gmail.com>
2016-10-27 17:59:55 -0500

    Updated gitignore and request_engine to have a global mlb enpoint string.

--- src/request_engine.py
+++ src/request_engine.py
@@ -1,17 +1,19 @@
 import requests, json, xml
 
+mlbEndpoint = 'http://gd.mlb.com'
+
 def getGames(date):
    games = []
 
-   scoreboard = requests.get('http://gd.mlb.com/components/game/mlb/year_' + date.strftime('%Y') + '/month_' + date.strftime('%m') + '/day_' + date.strftime('%d') + '/master_scoreboard.json')
+   scoreboard = requests.get(mlbEndpoint + '/components/game/mlb/year_' + date.strftime('%Y') + '/month_' + date.strftime('%m') + '/day_' + date.strftime('%d') + '/master_scoreboard.json')
    jsonSB = scoreboard.json()
    jsonGames = jsonSB['data']['games']
 
    for obj in jsonGames['game']:
-      print(obj['away_name_abbrev'] + ' @ ' + obj['home_name_abbrev'])
       games.append({ 'home': obj['home_name_abbrev'], 'away': obj['away_name_abbrev'],
                      'home_id': obj['home_team_id'], 'away_id': obj['away_team_id'],
                      'game_data_dir': obj['game_data_directory'] })
 
-   print(games)
    return games
+
+
