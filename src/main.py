97aba2442268d6a241325907793811ff6fe59eb7
Jon Catanio <joncatanio@gmail.com>
2016-10-27 18:45:39 -0500

    Restructured request_engine to return one content object. Now buildAppContent will go through the process of sifting through MLB API data and picking out and computing information we need. The API we will build wil use this in-memory model to return data.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from datetime import date
-import request_engine
 
 def main():
    curDate = date(2016, 6, 16)
-   request_engine.getGames(curDate)
+   appContent = request_engine.buildAppContent(curDate)
 
 main()
--- src/request_engine.py
+++ src/request_engine.py
@@ -2,18 +2,22 @@ import requests, json, xml
 
 mlbEndpoint = 'http://gd.mlb.com'
 
-def getGames(date):
-   games = []
-
+def getGames(content, date):
    scoreboard = requests.get(mlbEndpoint + '/components/game/mlb/year_' + date.strftime('%Y') + '/month_' + date.strftime('%m') + '/day_' + date.strftime('%d') + '/master_scoreboard.json')
    jsonSB = scoreboard.json()
    jsonGames = jsonSB['data']['games']
 
    for obj in jsonGames['game']:
-      games.append({ 'home': obj['home_name_abbrev'], 'away': obj['away_name_abbrev'],
+      content.append({ 'home': obj['home_name_abbrev'], 'away': obj['away_name_abbrev'],
                      'home_id': obj['home_team_id'], 'away_id': obj['away_team_id'],
                      'game_data_dir': obj['game_data_directory'] })
 
-   return games
+def getPitchersOfGames():
 
+   return
 
+def buildAppContent(date):
+   content = []
+   buildGames(content, date)
+
+   return content
