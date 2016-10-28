68e8ba286e1290aed07cc97ef9c07793d4cb0762
Jon Catanio <joncatanio@gmail.com>
2016-10-27 20:34:11 -0500

    Added xml parsing for endpoints.

--- src/request_engine.py
+++ src/request_engine.py
@@ -1,25 +1,40 @@
-import requests, json, xml
+import requests, json
+import xml.etree.ElementTree as ET
+from xml.dom.minidom import parse, parseString
 
 mlbEndpoint = 'http://gd.mlb.com'
 
 def getGames(content, date):
-   scoreboard = requests.get(mlbEndpoint + '/components/game/mlb/year_' + date.strftime('%Y') + '/month_' + date.strftime('%m') + '/day_' + date.strftime('%d') + '/master_scoreboard.json')
-   jsonSB = scoreboard.json()
-   jsonGames = jsonSB['data']['games']
+   res = requests.get(mlbEndpoint + '/components/game/mlb/year_' + date.strftime('%Y') + '/month_' + date.strftime('%m') + '/day_' + date.strftime('%d') + '/master_scoreboard.json')
+   if res.status_code != requests.codes.ok:
+      res.raise_for_status()
+
+   jsonRes = res.json()
+   jsonGames = jsonRes['data']['games']
 
    for obj in jsonGames['game']:
       content.append({ 'home': obj['home_name_abbrev'], 'away': obj['away_name_abbrev'],
                      'home_id': obj['home_team_id'], 'away_id': obj['away_team_id'],
                      'game_data_dir': obj['game_data_directory'] ,
-                     'rank_factors': { 'home': {}, 'away': {}}, 'date': date.isoformat()})
+                     'rank_factors': { 'home': [], 'away': []}})
 
-def getPitchingMatchup(content):
+def getPitchAndAvgData(content):
    matchup = []
-   players = requests.get(mlbEndpoint + content['game_data_dir'])
 
+   for game in content:
+      res = requests.get(mlbEndpoint + game['game_data_dir'] + '/players.xml')
+      if res.status_code != requests.codes.ok:
+         res.raise_for_status()
+
+      root = ET.fromstring(res.text)
+      teams = root.findall('team')
+      for team in teams:
+         for player in team.findall('player'):
+            player.get('boxname')
 
 def buildAppContent(date):
    content = []
    getGames(content, date)
+   getPitchAndAvgData(content)
 
    return content
