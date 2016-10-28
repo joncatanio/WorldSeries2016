a3574424b4af6ec22ce10df83c676c7881a7eccc
Jon Catanio <joncatanio@gmail.com>
2016-10-27 23:17:32 -0500

    Added team average, players average vs pitchers, and team averages vs pitchers

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from datetime import date
 import request_engine
 
 def main():
-   curDate = date(2016, 6, 16)
+   curDate = date(2016, 6, 17)
    appContent = request_engine.buildAppContent(curDate)
    print(appContent)
 
--- src/request_engine.py
+++ src/request_engine.py
@@ -1,5 +1,6 @@
 import requests, json
 import xml.etree.ElementTree as ET
+from decimal import *
 from xml.dom.minidom import parse, parseString
 
 mlbEndpoint = 'http://gd.mlb.com'
@@ -15,10 +16,81 @@ def getGames(content, date):
    for obj in jsonGames['game']:
       content.append({ 'home': obj['home_name_abbrev'], 'away': obj['away_name_abbrev'],
                      'home_id': obj['home_team_id'], 'away_id': obj['away_team_id'],
-                     'game_data_dir': obj['game_data_directory'] ,
+                     'game_data_dir': obj['game_data_directory'],
+                     'home_name': obj['home_team_name'], 'away_name': obj['away_team_name'],
                      'rank_factors': { 'home': [], 'away': []}})
 
+# Called by getPitchAndAvgData to determine if a player is very good against a pitcher
+def playerVsPitcher(content, game, team, player, pitcher):
+   res = requests.get('http://mlb.mlb.com/lookup/json/named.stats_batter_vs_pitcher_composed.bam?sport_code=\'mlb\'&game_type=\'R\'&player_id=\'' + player.get('id') + '\'&pitcher_id=\'' + pitcher.get('id') + '\'')
+   if res.status_code != requests.codes.ok:
+      res.raise_for_status()
+
+   obj = res.json()
+   info = obj['stats_batter_vs_pitcher_composed']['stats_batter_vs_pitcher_total']['queryResults']['row']
+
+   if info['ab'] == '' or int(info['ab']) < 5:
+      return None
+
+   getcontext().prec = 3
+   if int(info['ab']) >= 20:
+      if Decimal(info['avg']) > Decimal('.350'):
+         if team.get('id') == game['home']:
+            game['rank_factors']['home'].append({
+               'title': 'You\'re My Favorite',
+               'verbiage': info['player_first_last_html'] + ' is batting ' + info['avg'] + ' against today\'s pitcher.'
+            })
+         else:
+            game['rank_factors']['away'].append({
+               'title': 'You\'re My Favorite',
+               'verbiage': info['player_first_last_html'] + ' is batting ' + info['avg'] + ' against today\'s pitcher.'
+            })
+
+   return Decimal(info['avg'])
+
+def teamAvgThisYear(game, team, avg):
+   teamAvg = Decimal(avg) / Decimal(9)
+   if teamAvg > Decimal('0.300'):
+      if team.get('id') == game['home']:
+         game['rank_factors']['home'].append({
+            'title': 'High Average!',
+            'verbiage': 'The ' + game['home_name'] + ' have a batting average of ' + str(teamAvg)
+         })
+      else:
+         game['rank_factors']['away'].append({
+            'title': 'High Average!',
+            'verbiage': 'The ' + game['away_name'] + ' have a batting average of ' + str(teamAvg)
+         })
+
+def teamAverageVsPitcher(game, team, faced, avgAgainstP, pitcher):
+   if faced > 4:
+      avgAgainstP = avgAgainstP / Decimal(faced)
+      if avgAgainstP < Decimal('0.150'):
+         if team.get('id') == game['home']:
+            game['rank_factors']['home'].append({
+               'title': 'Dominant Pitcher',
+               'verbiage': pitcher.get('first') + ' ' + pitcher.get('last') + ' holds the ' + game['home_name'] + ' to a team batting average of ' + str(avgAgainstP)
+            })
+         else:
+            game['rank_factors']['away'].append({
+               'title': 'Dominant Pitcher',
+               'verbiage': pitcher.get('first') + ' ' + pitcher.get('last') + ' holds the ' + game['away_name'] + ' to a team batting average of ' + str(avgAgainstP)
+            })
+      if avgAgainstP >= Decimal('0.325'):
+         if team.get('id') == game['home']:
+            game['rank_factors']['home'].append({
+               'title': 'Our Favorite Pitcher',
+               'verbiage': 'The ' + game['home_name'] + ' hit ' + str(avgAgainstP) + ' against ' + pitcher.get('first') + ' ' + pitcher.get('last')
+            })
+         else:
+            game['rank_factors']['away'].append({
+               'title': 'Our Favorite Pitcher',
+               'verbiage': 'The ' + game['away_name'] + ' hit ' + str(avgAgainstP) + ' against ' + pitcher.get('first') + ' ' + pitcher.get('last')
+            })
+
 def getPitchAndAvgData(content):
+   # For adding batting averages 
+   getcontext().prec = 3
    matchup = []
 
    for game in content:
@@ -28,9 +100,59 @@ def getPitchAndAvgData(content):
 
       root = ET.fromstring(res.text)
       teams = root.findall('team')
+
+      awayP = None
+      homeP = None
       for team in teams:
-         for player in team.findall('player'):
-            player.get('boxname')
+         players = team.findall('player')
+         for player in players:
+            if player.get('game_position') == 'P':
+               if team.get('type') == 'away':
+                  awayP = player
+               else:
+                  homeP = player
+
+      game['away_pitcher'] = awayP.get('first') + ' ' + awayP.get('last')
+      game['home_pitcher'] = homeP.get('first') + ' ' + homeP.get('last')
+
+      for team in teams:
+         players = team.findall('player')
+         avg = Decimal('0.0')
+         avgAgainstP = Decimal('0.0')
+         pitcherAvg = Decimal('0.0')
+         starters = 0
+         faced = 0
+
+         # Find the pitcher for playerVsPitcher()
+         pitcher = None
+         if team.get('type') == 'away':
+            pitcher = homeP
+         else:
+            pitcher = awayP
+
+         for player in players:
+            pos = player.get('game_position')
+            starters += 1  
+
+            if pos != None:
+               if pos == 'P':
+                  pitcherAvg = Decimal(player.get('avg'))
+               else:
+                  # The player is a starter (non-pitcher)
+                  tempAvg = playerVsPitcher(content, game, team, player, pitcher)
+                  if tempAvg != None:
+                     avgAgainstP += tempAvg
+                     faced += 1
+
+                  avg += Decimal(player.get('avg'))
+
+         # Team average overall (on the season)
+         if starters == 9:
+            avg += pitcherAvg
+         teamAvgThisYear(game, team, avg)
+
+         # Team average vs pitcher
+         teamAverageVsPitcher(game, team, faced, avgAgainstP, pitcher)
 
 def buildAppContent(date):
    content = []
