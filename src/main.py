642d93f2290c6fc3d8323e65dfcee48b65618ea5
Giancarlo Tarantino <Giancarlo@Giancarlos-MacBook-Air.local>
2016-10-28 04:54:49 -0500

    Added standing comparsions (season and wildcard after month 7)

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from datetime import date
 import request_engine
 
 def main():
-   curDate = date(2016, 9, 27)
+   curDate = date(2016, 8, 21)
    appContent = request_engine.buildAppContent(curDate)
    print(appContent)
 
--- src/request_engine.py
+++ src/request_engine.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 from team_streaks import getWinLoseStreak
 from batting_pitching import getPitchAndAvgData
 from rivalry_factor import getRivalries
+from standings_comparison import getStandingsComparison
 from thresholds_globals import *
 
 def getGames(content, date):
@@ -13,6 +14,8 @@ def getGames(content, date):
    jsonRes = res.json()
    jsonGames = jsonRes['data']['games']
 
+   month = jsonGames['month']
+
    for obj in jsonGames['game']:
       content.append({ 'home': obj['home_name_abbrev'], 'away': obj['away_name_abbrev'],
                      'home_id': obj['home_team_id'], 'away_id': obj['away_team_id'],
@@ -21,6 +24,11 @@ def getGames(content, date):
                      'rank_factors': { 'home': [], 'away': []}, 'est_time': obj['time'],
                      'home_win': obj['home_win'], 'home_loss': obj['home_loss'],
                      'away_win': obj['away_win'], 'away_loss': obj['away_loss'],
+                     'home_games_back_wildcard': obj['home_games_back_wildcard'],
+                     'home_games_back': obj['home_games_back'],
+                     'away_games_back_wildcard': obj['away_games_back_wildcard'],
+                     'away_games_back': obj['away_games_back'],
+                     'month': month,
                      'taglines': []})
 
 
@@ -30,5 +38,6 @@ def buildAppContent(date):
    getWinLoseStreak(content, date)
    getPitchAndAvgData(content)
    getRivalries(content)
+   getStandingsComparison(content)
 
    return content
